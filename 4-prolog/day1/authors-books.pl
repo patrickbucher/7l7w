75cd5b48ce19ee2b072c53ae0294158c169d9aff
patrickbucher <patrick.bucher@stud.hslu.ch>
2018-09-30 08:36:45 +0200

    renamed folders

---
+++ 4-prolog/day1/authors-books.pl
@@ -0,0 +1,21 @@
+writtenBy('Thomas Bernhard', 'Frost').
+writtenBy('Thomas Bernhard', 'Verstörung').
+writtenBy('Thomas Bernhard', 'Das Kalkwerk').
+writtenBy('Thomas Bernhard', 'Korrektur').
+writtenBy('Thomas Bernhard', 'Beton').
+writtenBy('Thomas Bernhard', 'Der Untergeher').
+writtenBy('Thomas Bernhard', 'Holzfällen').
+writtenBy('Thomas Bernhard', 'Alte Meister').
+writtenBy('Thomas Bernhard', 'Auslöschung').
+
+writtenBy('Michel Houellebecq', 'Elementarteilchen').
+writtenBy('Michel Houellebecq', 'Ausweitung der Kampfzone').
+
+writtenBy('Lev Tolstoj', 'Krieg und Frieden').
+writtenBy('Lev Tolstoj', 'Anna Karenina').
+
+writtenBy('Fëdor Dostoevskij', 'Verbrechen und Strafe').
+writtenBy('Fëdor Dostoevskij', 'Die Brüder Karamasow').
+
+% Query
+% writtenBy('Thomas Bernhard', Book).
---
+++ 4-prolog/day1/musicians-instruments.pl
@@ -0,0 +1,12 @@
+plays('Dave Murray', 'Guitar').
+plays('Jannick Gers', 'Guitar').
+plays('Adrian Smith', 'Guitar').
+plays('Steve Harris', 'Bass').
+plays('Nicko McBrain', 'Drums').
+plays('Jim Matheos', 'Guitar').
+plays('Glenn Tipton', 'Guitar').
+plays('Scott Travis', 'Drums').
+plays('Ian Hill', 'Bass').
+
+% Query
+% plays(Musician, 'Guitar').
